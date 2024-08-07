2ccc4a2702c8f08913972cb4f0ae2e38855cd2b2
Sergeiphantom86 <96051914+Sergeiphantom86@users.noreply.github.com>
2024-08-07 15:44:59 +0300

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
+[RequireComponent (typeof(PlayerMover))]
+
 public class Player : MonoBehaviour
 {
-    [SerializeField] private PlayerController _controller;
+    public const string Horizontal = nameof(Horizontal);
+    public const string Speed = nameof(Speed);
+    public const string Jump = nameof(Jump);
+
+    [SerializeField] private PlayerMover _playerMover;
     [SerializeField] private Animator _animator;
     [SerializeField] private float _runSpeed = 40f;
 
     private float _horizontalMove = 0;
     private bool _jump = false;
-    public const string Speed = nameof(Speed);
-    public const string Horizontal = nameof(Horizontal);
-    public const string Jump = nameof(Jump);
 
     private void Update()
     {
@@ -26,7 +29,9 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _controller.Move(_horizontalMove * Time.fixedDeltaTime, _jump);
+        _playerMover.Move(_horizontalMove * Time.fixedDeltaTime);
+
+        _playerMover.Jump(_jump);
 
         _jump = false;
     }
